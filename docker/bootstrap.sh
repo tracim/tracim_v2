323429483a853bc18dc1a89a37fb2b067c719acf
Bastien Sevajol <bastien.sevajol@algoo.fr>
2018-10-08 17:53:04 +0200

    fix docker bootstrap.sh

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -2,12 +2,12 @@
 
 # If no config file, create it
 if [ ! -f /etc/tracim/config.ini ]; then
-    cp /opt/tracim/backend/development.ini.sample /etc/config.ini
+    cp /opt/tracim/backend/development.ini.sample /etc/tracim/config.ini
     # Must prepare some lines to be able to use tracim on 127.0.0.1 (and 127.0.0.1 only !)
-    sed -i "s|listen = .*|listen = 0.0.0.0:6543|g" /etc/config.ini
-    sed -i "s|website.base_url = .*|website.base_url = http://localhost:6543|g" development.ini
+    sed -i "s|listen = .*|listen = 0.0.0.0:6543|g" /etc/tracim/config.ini
+    sed -i "s|website.base_url = .*|website.base_url = http://localhost:6543|g" /etc/tracim/config.ini
     # TODO BS 2018-10-08: Don't touch yet, it need a rebuild of js. If want to change it, keep in mind !!
-    sed -i "s|\"apiUrl\": \"http://localhost:6543/api/v2\"|\"apiUrl\": \"http://localhost:6543/api/v2\"|g" configEnv.json
+    # sed -i "s|\"apiUrl\": \"http://localhost:6543/api/v2\"|\"apiUrl\": \"http://localhost:6543/api/v2\"|g" configEnv.json
     # TODO 2018-10-08: Must do some other things ... secret etc
 fi
 
