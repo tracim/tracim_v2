83f91e2192572d30903d620e478fae5dcf89cb98
Bastien Sevajol <bastien.sevajol@algoo.fr>
2018-10-08 16:30:26 +0200

    complete docker bash script

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,20 @@ ENV LC_ALL C.UTF-8
 
 COPY . /opt/tracim
 
+# Install deps
 RUN apt-get update && \
      DEBIAN_FRONTEND=noninteractive apt-get install -y sudo curl wget
+
+# Tracim install
+# TODO BS 2018-10-08: Add step to reduce final disk size
 RUN cd /opt/tracim && \
     DEBIAN_FRONTEND=noninteractive ./setup_default_backend.sh && \
     DEBIAN_FRONTEND=noninteractive ./install_frontend_dependencies.sh && \
     DEBIAN_FRONTEND=noninteractive ./build_full_frontend.sh
+
+# Container structure
+RUN mkdir /etc/tracim
 COPY docker/bootstrap.sh /
+WORKDIR /opt/tracim/backend
+VOLUME ["/etc/tracim", "/var/lib/tracim"]
+CMD ["/bin/bash", "/bootstrap.sh"]
--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-cd /tracim/backend/
+# If no config file, create it
+if [ ! -f /etc/tracim/config.ini ]; then
+    cp /opt/tracim/backend/development.ini.sample /etc/config.ini
+    # Must prepare some lines to be able to use tracim on 127.0.0.1 (and 127.0.0.1 only !)
+    sed -i "s|listen = .*|listen = 0.0.0.0:6543|g" /etc/config.ini
+    sed -i "s|website.base_url = .*|website.base_url = http://localhost:6543|g" development.ini
+    # TODO BS 2018-10-08: Don't touch yet, it need a rebuild of js. If want to change it, keep in mind !!
+    sed -i "s|\"apiUrl\": \"http://localhost:6543/api/v2\"|\"apiUrl\": \"http://localhost:6543/api/v2\"|g" configEnv.json
+    # TODO 2018-10-08: Must do some other things ... secret etc
+fi
+
+cd /opt/tracim/backend/
 source env/bin/activate
-pserve development.ini
+# TODO BS 2018-10-08: More strong http server
+pserve /etc/tracim/config.ini
