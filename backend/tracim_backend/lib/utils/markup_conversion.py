52603ba687aa3ad5d7a3546da9d73e95c24ed341
Guénaël Muller <guenael.muller@algoo.fr>
2018-10-30 15:03:25 +0100

    clean up convert to another_markup function

--- backend/tracim_backend/lib/utils/markup_conversion.py
+++ backend/tracim_backend/lib/utils/markup_conversion.py
@@ -3,10 +3,17 @@ HTML_MARKUP = 'html'
 
 
 def convert_to_another_markup(
-        content,
+        content: str,
         input_markup: str,
         output_markup: str,
 ) -> str:
+    """
+
+    :param content: content as string
+    :param input_markup: valid markup langage slug
+    :param output_markup: valid markup langage slug
+    :return: converted_content as string
+    """
     # TODO - G.M - 2018-10-29 - Support for html content, deal correctly
     # with line break.
     if input_markup == TEXT_MARKUP and output_markup == HTML_MARKUP:
